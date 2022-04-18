a9621eb0fdc7daf6507eb50e24073b1d2c1cd187
emaci <emaci@DESKTOP-EDU>
2022-04-18 20:07:40 -0300

    conversor de gregoriano para juliado criado

--- src/controller/ConversorData.java
+++ src/controller/ConversorData.java
@@ -1,5 +1,26 @@
 package controller;
 
 public class ConversorData {
+	
+	private int ano;
+	private int mes;
+	private int dia;
+	private int diaJuliano;
+	
+	public int dataJuliana(int dia, int mes, int ano){		
+		this.ano = ano;
+		this.mes = mes;
+		this.dia = dia;
+		
+		diaJuliano = (1461 * (ano + 4800 +(mes -14)/12)) / 4 + (367 * (mes - 2 -12 *((mes -14)/12)))/12 - (3 * ((ano +4900+(mes -14)/12)/100))/4 + dia -32075;
+		
+		/**diaJuliano= ( 1461 * ( ano + 4800 + ( mes - 14)/12 )  ) / 4 +
+
+                ( 367 * ( mes - 2- 12 * (  ( mes - 14)/12 )  ) ) /12 -
+
+                ( 3 * ( ( ano + 4900 + ( mes - 14 ) / 12 ) / 100 )  ) / 4 + dia - 32075;**/
+		
+		return diaJuliano;
+	}
 
 }
--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,14 @@
 package view;
 
+import controller.ConversorData;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ConversorData dataGregoriana = new ConversorData();
+		
+		System.out.println(dataGregoriana.dataJuliana(5, 8, 2016));
 
 	}
 
